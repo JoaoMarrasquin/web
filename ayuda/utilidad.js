36e840757d3a1c9e6808e90c6a39cc5888201f0c
JoaoMarrasquin <56419166+JoaoMarrasquin@users.noreply.github.com>
2021-02-28 01:44:08 -0500

    Add files via upload

---
+++ ayuda/utilidad.js
@@ -0,0 +1,23 @@
+//
+'use strict'
+
+function printError(res, error) {
+    try{
+        console.log(error.msg)
+        res.json(error)
+    } catch (error){
+        // no pasa nada
+    }
+
+}
+
+function returnPromise(condicion, verdadero, falso) {
+    return new Promise((resolve, reject) =>{
+        return(condicion) ? resolve(verdadero) : reject(falso)
+    })
+}
+
+module.exports = {
+    printError,
+    returnPromise
+}
